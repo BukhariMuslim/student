a2698baf8d9dfaae582ad1d507f1ace8e1f01d8d
BukhariMuslim <bm.bukharimuslim@gmail.com>
2017-11-07 23:00:57 +0700

    Update Payment Seeder

--- database/seeds/PaymentTableSeeder.php
+++ database/seeds/PaymentTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class PaymentTableSeeder extends Seeder
 {
@@ -11,6 +12,47 @@ class PaymentTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('payments')->insert(
+            [
+                'code' => str_random(10),
+                'student_id' => 1,
+                'course_id' => 1,
+                'instructor_id' => 1,
+                'amount' => 300000,
+                'date' => new Carbon(),
+            ],
+            [
+                'code' => str_random(10),
+                'student_id' => 3,
+                'course_id' => 2,
+                'instructor_id' => 3,
+                'amount' => 350000,
+                'date' => new Carbon(),
+            ],
+            [
+                'code' => str_random(10),
+                'student_id' => 1,
+                'course_id' => 2,
+                'instructor_id' => 3,
+                'amount' => 350000,
+                'date' => new Carbon(),
+            ],
+            [
+                'code' => str_random(10),
+                'student_id' => 2,
+                'course_id' => 2,
+                'instructor_id' => 3,
+                'amount' => 350000,
+                'date' => new Carbon(),
+            ],
+            [
+                'code' => str_random(10),
+                'student_id' => 1,
+                'course_id' => 2,
+                'instructor_id' => 4,
+                'amount' => 320000,
+                'date' => new Carbon(),
+            ]
+        );
     }
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 # Student
 
 Sample Project of student enrollment
+
+## Instalation
+Run `php artisan migrate` to initiate data
